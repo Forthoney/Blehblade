7c83224f4611539bc656ba9e28e423e687c631ee
Seong-Heon (Sunghun) Jung <castlehoneyjung@gmail.com>
2022-09-24 15:56:36 -0400

    add interaction starters and subject lists to item interactions

--- Assets/ItemInteractions.cs
+++ Assets/ItemInteractions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Interactions", menuName = "ScriptableObjects/Interactions", order = 1)]
-public class Stage1Interaction : ScriptableObject
+public class ItemInteractions : ScriptableObject
 {
     public List<GameObject> interactionStarter;
     public List<GameObject> interactionSubject;
