2b6e65510c0a94ea8f20a54559ddea172a48e51b
jabramson24 <joshua_abramson@brown.edu>
2022-10-15 12:21:01 -0400

    removed test code
    contains only changeScreens function

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -6,27 +6,8 @@ using UnityEngine.SceneManagement;
 public class SceneController : MonoBehaviour
 {
 
-
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown("space"))
-        {
-            changeScreens("PrototypeStage");
-        } else if (Input.GetKeyDown("f"))
-        {
-            changeScreens("SampleScene");
-        }
-        
-    }
-
-    public void changeScreens(string newScene)
+    //receives a string representing the name of the new scene, and loads it
+    public void changeScreens(string newScene)      
     {
         SceneManager.LoadScene(newScene);
     }
