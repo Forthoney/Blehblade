32ff1e3c126160291d0fa2af5880c62017ed0903
Seong-Heon (Sunghun) Jung <castlehoneyjung@gmail.com>
2022-10-15 20:57:00 -0400

    Fix drag and drop bug and modify scene to better reflect inventory system in development

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -2,19 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class InteractableObject : MonoBehaviour
 {
     [SerializeField] private float inventoryZ = 1;
-    [SerializeField] private string itemName; // Do we need this field?
     [SerializeField] private string targetColliderName;
     [SerializeField] private List<GameObject> triggerObjects;
     [SerializeField] private float speedDuringActivation = 1.0f;
     [SerializeField] private float speedDuringDragging = 2.0f;
 
     public Vector3 inventoryPos; // TODO: Change this to private once the inventory manager script is finished
-    private bool _inTargetObject = false;
+    private bool _onTarget = false;
     // A tuple of representing the state of the object. It consists of the object's activation status and movement.
+    // You can tell I was certainly inspired by CS1010's Finite State Machines
     private (Status, Movement) _state = (Status.Inactive, Movement.Stationary);
     private readonly Vector3 _centerOfScreen = new Vector3(0.0f, 0.8f, -10.0f);
 
@@ -33,7 +34,6 @@ public class InteractableObject : MonoBehaviour
     private enum Movement
     {
         Dragging,
-        Dropping,
         // Returning to inventory
         Returning,
         Stationary,
@@ -46,7 +46,6 @@ public class InteractableObject : MonoBehaviour
             (Status.Activating, _) => Move(_centerOfScreen, speedDuringActivation),
             (Status.Stashing, _) => Move(inventoryPos, speedDuringActivation),
             (Status.Active, Movement.Dragging) => CalcMousePos(),
-            (Status.Active, Movement.Dropping) => Move(CalcMousePos(), speedDuringDragging),
             (Status.Active, Movement.Returning) => Move(inventoryPos, speedDuringDragging),
             _ => transform.position
         };
@@ -60,7 +59,7 @@ public class InteractableObject : MonoBehaviour
                 _state.Item1 = Status.Activating;
                 break;
             case Status.Active:
-                _state.Item2 = Movement.Dropping;
+                _state.Item2 = Movement.Dragging;
                 gameObject.GetComponent<BoxCollider>().enabled = true;
                 break;
             case Status.Stashing:
@@ -74,54 +73,50 @@ public class InteractableObject : MonoBehaviour
     
     private void OnMouseUp()
     {
-        if (_state == (Status.Active, Movement.Dragging))
-        {
-            _state.Item2 = Movement.Returning;
-            if (!_inTargetObject) return;
+        if (_state != (Status.Active, Movement.Dragging)) return;
         
-            // If object hits the intended collider
-            EventController.Instance.PlayerUse(gameObject);
-            foreach (var obj in triggerObjects)
-            {
-                obj.SetActive(true);
-            }
-            Debug.Log("Trigger Event!");
-        }
+        _state.Item2 = Movement.Returning;
+        if (!_onTarget) return;
+        
+        EventController.Instance.PlayerUse(gameObject);
+        triggerObjects.ForEach(obj => obj.SetActive(true));
+        Debug.Log("Trigger Event!");
     }
 
     private Vector3 Move(Vector3 dest, float speed)
     {
-        // Finished moving
-        if (Vector3.Distance(transform.position, dest) < 0.001f)
+        if (Vector3.Distance(transform.position, dest) > 0.001f)
         {
-            _state = _state switch
-            {
-                (Status.Activating, _) => (Status.Stashing, _state.Item2),
-                (Status.Stashing, _) => (Status.Active, _state.Item2),
-                (_, Movement.Dropping) => (_state.Item1, Movement.Dragging),
-                (_, Movement.Returning) => (_state.Item1, Movement.Stationary),
-                _ => _state
-            };
-            return transform.position;
+            var step = speed * Time.deltaTime;
+            return Vector3.MoveTowards(transform.position, dest, step);
         }
         
-        var step = speed * Time.deltaTime;
-        return Vector3.MoveTowards(transform.position, dest, step);
+        // If finished moving, change state but not transform
+        _state = _state switch
+        {
+            (Status.Activating, _) => (Status.Stashing, _state.Item2),
+            (Status.Stashing, _) => (Status.Active, _state.Item2),
+            (_, Movement.Returning) => (_state.Item1, Movement.Stationary),
+            _ => _state
+        };
+        return transform.position;
     }
     
     void OnTriggerEnter(Collider other)
     {
+        Assert.AreNotEqual("", targetColliderName);
         if (other.name == targetColliderName)
         {
-            _inTargetObject = true;
+            _onTarget = true;
         }
     }
 
     void OnTriggerExit(Collider other)
     {
+        Assert.AreNotEqual("", targetColliderName);
         if (other.name == targetColliderName)
         {
-            _inTargetObject = false;
+            _onTarget = false;
         }
     }
 
