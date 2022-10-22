451d391a042649814ffb2ced4dbfe64e3847759a
Seong-Heon (Sunghun) Jung <castlehoneyjung@gmail.com>
2022-10-22 14:19:48 -0400

    refine beyblade script and prefabs

--- Assets/Scripts/Beyblade.cs
+++ Assets/Scripts/Beyblade.cs
@@ -4,38 +4,20 @@ using UnityEngine;
 
 public class Beyblade : MonoBehaviour
 {
-    [SerializeField] private Vector3 initSpeed;
-    private bool gameRunning = false;
-    [SerializeField] private GameObject eventControl;
     [SerializeField] private bool isPlayer;
     [SerializeField] private float initialVelocity;
     private float _currentVelocity;
-
-    // Physics
     private Rigidbody _rigidbody;
-
-    // Keystrokes
-    private bool spacePressed = false;
-    private bool wPressed = false;
-    private bool sPressed = false;
-    private bool aPressed = false;
-    private bool dPressed = false;
-
-
-
-
-
-
-    // Start is called before the first frame update
 
     public void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
+    
     public void StartBeyblade()
     {
         _rigidbody.isKinematic = false;
-        _currentVelocity = initialVelocity / 3;
+        _currentVelocity = isPlayer? initialVelocity / 10 : initialVelocity / 20;
         AddRandomForce(initialVelocity);
     }
 
@@ -48,7 +30,6 @@ public class Beyblade : MonoBehaviour
 
     private void AddRandomForce(float velocity)
     {
-        Debug.Log(velocity);
         var randomForce = Random.insideUnitCircle * velocity;
         _rigidbody.AddForce(new Vector3(randomForce.x, 0, randomForce.y));
     }
