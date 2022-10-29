fc4bb10ca7cd94a1de171b897aa27dc29d078c1a
Timfon <timmyoctoidea@gmail.com>
2022-10-29 15:50:37 -0400

    Added scrolling credits scene

---
+++ Assets/Scripts/ScrollCredit.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollCredit : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    // Start is called before the first frame update
+    RectTransform m_RectTransform;
+
+    void Start()
+    {
+        //Fetch the RectTransform from the GameObject
+        m_RectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_RectTransform.anchoredPosition += new Vector2(0, speed);
+    }
+}
